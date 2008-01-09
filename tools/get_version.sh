86adf63aeb0f7734559d55102434a151a2a9e770
glashan <glashan@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2008-01-09 22:01:33 +0000

    updated properties and doxyfile
    git-svn-id: http://joyos.googlecode.com/svn/trunk@73 d6c55bfe-47cd-0eed-8827-52b0d3839261

--- tools/get_version.sh
+++ tools/get_version.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# Print out a JoyOS/Happyboard version string for Doxygen
+# Print out a JoyOS version string for Doxygen
 
 JOYOS_VER=`cat src/inc/kern/global.h | grep "VERSION" | cut -f 2 -d "\""`
-HAPPY_VER=`cat src/inc/config.h | grep "VERSION_STRING" | cut -f 2 -d "\""`
 SVN_VER=`svn info | grep "Revision" | cut -f 2 -d " "`
 
-VER_STRING="v$JOYOS_VER (b$SVN_VER/$HAPPY_VER)"
+VER_STRING="v$JOYOS_VER (b$SVN_VER)"
 echo "PROJECT_NUMBER=\"$VER_STRING\""
