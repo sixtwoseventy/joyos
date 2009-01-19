877ded77fa4d419d860308a334cc0e1f3afdc291
sgherbst <sgherbst@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2009-01-19 04:18:24 +0000

    updated gyrotest to use LSB_US_PER_DEG instead of LSB_MS_PER_DEG, incremented version number and rolled out a new release for contestants
    git-svn-id: http://joyos.googlecode.com/svn/trunk@116 d6c55bfe-47cd-0eed-8827-52b0d3839261

--- user/gyrotest/umain.c
+++ user/gyrotest/umain.c
@@ -35,7 +35,9 @@
 #include <joyos.h>
 
 #define GYRO_PORT 		11
-#define LSB_MS_PER_DEG	1062.0
+#define LSB_US_PER_DEG	1400000
+
+uint8_t team_number={1,0};
 
 // usetup is called during the calibration period. It must return before the
 // period ends.
@@ -45,7 +47,7 @@ int usetup (void) {
 	printf_P (PSTR("\nStabilizing..."));
 	pause (500);
 	printf_P (PSTR("\nCalibrating     offset...\n"));
-	gyro_init (GYRO_PORT, LSB_MS_PER_DEG, 500L);
+	gyro_init (GYRO_PORT, LSB_US_PER_DEG, 500L);
 	return 0;
 }
 
