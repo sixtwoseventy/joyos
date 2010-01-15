196d686dd8f9ecb030c40fd027534dfb6c594708
sgherbst <sgherbst@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2010-01-15 04:17:09 +0000

    needed for last checkin.
    git-svn-id: http://joyos.googlecode.com/svn/trunk@192 d6c55bfe-47cd-0eed-8827-52b0d3839261

---
+++ src/inc/packet.h
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define PAYLOAD_SIZE 30
+
+#define POSITION 0x00 //Updated position of this bot, other bot, or mouse
+#define START 0x01 //Start of the round
+#define STOP 0x02 //End of the round
+#define STRING 0x03 //String from bot to board
+#define SYNC 0x04 //Not sure how to use yet.  Established IDs of all bots/vision system on board
+#define GOAL 0x05 //Sets the target position of this robot
+#define LIGHT 0x06 //Instructs robots to turn their lights on or off
+
+typedef struct {
+	uint8_t id;
+	signed x : 12;
+	signed y : 12;
+	signed theta : 12;
+	unsigned confidence : 12;
+} __attribute__ ((aligned (1))) __attribute__ ((packed)) board_coord;
+
+typedef struct{
+    uint8_t id;
+    int16_t value;
+} __attribute__ ((aligned (1))) __attribute__ ((packed)) light_command;
+
+typedef struct{
+	uint8_t type;
+	uint8_t address;
+	uint8_t payload[PAYLOAD_SIZE];
+} packet;
+
+typedef struct {
+	uint8_t type;
+	uint8_t address;
+	union {
+		uint8_t array[PAYLOAD_SIZE];
+		board_coord coords[4];
+        light_command lights[4];
+	} payload;
+} __attribute__ ((packed)) packet_buffer;
+
