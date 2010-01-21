b49e1bc0a19c0024cade4b68766c1932e83953c4
vaumnou <vaumnou@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2010-01-21 16:37:10 +0000

    Added #ifndef to prevent multiple inclusion.
    git-svn-id: http://joyos.googlecode.com/svn/trunk@224 d6c55bfe-47cd-0eed-8827-52b0d3839261

--- src/inc/kern/ring.h
+++ src/inc/kern/ring.h
@@ -1,3 +1,6 @@
+#ifndef __INCLUDE_RING_H__
+#define __INCLUDE_RING_H__
+
 #include <kern/lock.h>
 
 typedef struct {
@@ -12,3 +15,5 @@ void ring_init(ring_buf *ring, uint16_t size);
 uint16_t ring_write(ring_buf *ring, uint8_t *buf, uint16_t count);
 uint16_t ring_read(ring_buf *ring, uint8_t *buf, uint16_t count);
 uint16_t ring_size(ring_buf *ring);
+
+#endif
