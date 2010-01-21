57e72f7b4bffb5d3c771575b9b078ea3309d1f99
sgherbst <sgherbst@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2010-01-21 22:33:26 +0000

    added ram test
    git-svn-id: http://joyos.googlecode.com/svn/trunk@231 d6c55bfe-47cd-0eed-8827-52b0d3839261

---
+++ tests/ramtest.c
@@ -0,0 +1,62 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2007 MIT 6.270 Robotics Competition
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+// Include headers from OS
+#include <joyos.h>
+
+// usetup is called during the calibration period. It must return before the
+// period ends.
+int usetup (void) {
+	return 0;
+}
+
+// Entry point to contestant code.
+// Create threads and return 0.
+
+#define TEST_SIZE 0x7FF0
+
+int
+umain (void) {	
+
+	uint8_t* ptr = malloc(TEST_SIZE);
+
+	if (!ptr) panic("malloc() failed.");
+	
+	uint16_t index;
+	for(index=0;index<TEST_SIZE;index++){
+		ptr[index]=(uint8_t)index;
+	}
+
+	for(index=0;index<TEST_SIZE;index++){
+		if (ptr[index] != (uint8_t)index){
+			panic("RAM Test Error.");
+		}
+	}
+
+	printf("RAM Test Successful.\n");
+
+	return 0;
+}
+
