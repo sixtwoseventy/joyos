5113e703b81e778eecfe1f84d74ec376feae772b
darkomega1231 <darkomega1231@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2010-01-22 02:39:00 +0000

    Updated mice, added mouse which can use RF to get to another mouse, as a waypoint
    git-svn-id: http://joyos.googlecode.com/svn/trunk@233 d6c55bfe-47cd-0eed-8827-52b0d3839261

---
+++ user/mouse/mouseRun.c
@@ -0,0 +1,229 @@
+#include <joyos.h>
+#include <math.h>
+#include <lib/pid.h>
+
+#define GYRO_PORT 		11
+//Negative to read CW angle
+#define LSB_US_PER_DEG	-1400000*365.0/360.0
+
+#define DRIVE_KP 2.0
+#define DRIVE_KD 0.0
+#define DRIVE_KI 0.1
+
+#define RIGHT_MOTOR 0
+#define LEFT_MOTOR 1
+#define MAX_VEL 150
+
+#define TURN_THRESH 5
+#define TURN_VEL 60
+
+#define CLAMP(X) X>0?(X>MAX_VEL?MAX_VEL:X):0
+#define REV_CLAMP(X) X<0?(X<-MAX_VEL?-MAX_VEL:X):0
+#define RF_TO_DEGREES(X) (float)(X*180.0/2048.0)
+#define RAD_TO_DEGREES(X) (float)(X*180.0/M_PI)
+#define CORRECT_ANGLE(X) (X+180)%360-180
+#define RF_TO_INCHES(X) X*96.0/4096.0
+
+typedef enum {
+	TURNING,
+	DRIVING,
+	INIT,
+	ACTIVATED,
+	HALT
+} state_enum;
+
+state_enum state = INIT;
+
+int x = 0;
+int y = 0;
+float angle; //CW about +y
+
+int target_x = 0;
+int target_y = 0;
+float angle_to_target = 0;
+
+struct pid_controller driver;
+
+int targetVel = 40;
+
+uint32_t state_time = 0;
+
+uint32_t last_update_time = 0;
+
+void updateSelfPosition(bool update_gyro);
+
+void applyMotorDifferential(float vel) {
+	int intVel = (int)vel;
+	//printf("\nR:%i, L:%i, %i",(-targetVel + intVel), (-targetVel-intVel), (int16_t)REV_CLAMP(-targetVel + intVel));
+	motor_set_vel(RIGHT_MOTOR,(int16_t)REV_CLAMP(-targetVel - intVel));
+	motor_set_vel(LEFT_MOTOR,(int16_t)REV_CLAMP(-targetVel + intVel));
+}
+
+void resetPID(float target) {
+	init_pid(&driver,
+			DRIVE_KP,
+			DRIVE_KI,
+			DRIVE_KD,
+			&gyro_get_degrees,
+			&applyMotorDifferential);
+	driver.goal = target;
+	driver.enabled = 1;
+}
+
+// usetup is called during the calibration period. It must return before the
+// period ends.
+int usetup (void) {
+	extern volatile uint8_t robot_id;
+	robot_id = 64;
+	extern volatile uint8_t light_port;
+	light_port = 2;
+	
+	printf("\nPlace robot,    press go.");
+	go_click ();
+	printf ("\nStabilizing...");
+	pause (500);
+	printf ("\nCalibrating     offset...\n");
+	gyro_init (GYRO_PORT, LSB_US_PER_DEG, 500L);
+	
+	resetPID(0.0f);
+	
+	return 0;
+}
+
+
+float getTargetAngle(int x1, int y1) {
+	//Argument order switched to create CW angle about +y
+	return RAD_TO_DEGREES(atan2((float)(x1-x),(float)(y1-y)));
+}
+
+float getDistanceToTarget(int x1, int y1) {
+	float diffx = (float)(x1-x);
+	float diffy = (float)(y1-y);
+	return RF_TO_INCHES((float)sqrt(diffx*diffx+diffy*diffy));
+}
+
+//float getTurnAmount(int x1, int y1) {
+//	
+//}
+
+bool turnToTarget() {
+//return true if need to be called next frame
+	float angle_difference = angle_to_target-gyro_get_degrees();
+	int int_diff = (int)angle_difference;
+	while (int_diff < 0) {
+		int_diff += 360;
+	}
+	//printf("\n%.2f, %.2f, %i", angle, angle_to_target, int_diff);
+	int_diff = CORRECT_ANGLE(int_diff);
+	if (int_diff > TURN_THRESH) {
+		motor_set_vel(RIGHT_MOTOR, -TURN_VEL);
+		motor_set_vel(LEFT_MOTOR, TURN_VEL);
+		return true;
+	} else if (int_diff < -TURN_THRESH) {
+		motor_set_vel(RIGHT_MOTOR, TURN_VEL);
+		motor_set_vel(LEFT_MOTOR, -TURN_VEL);
+		return true;
+	} else {
+		motor_set_vel(RIGHT_MOTOR, 0);
+		motor_set_vel(LEFT_MOTOR, 0);
+		return false;
+	}
+}
+
+bool driveToTarget() {
+//return true if need to be called next frame
+	if (get_time() - state_time > 3500) {
+		updateSelfPosition(true);
+		state_time = get_time();
+	}
+	else if (get_time() - state_time > 3000) {
+		motor_set_vel(RIGHT_MOTOR,0);
+		motor_set_vel(LEFT_MOTOR,0);
+		return true;
+	}
+	if (get_time() - last_update_time > 500) {
+		updateSelfPosition(false);
+	}
+	if (getDistanceToTarget(target_x,target_y) < 12.0) {
+		motor_set_vel(RIGHT_MOTOR,0);
+		motor_set_vel(LEFT_MOTOR,0);
+		return false;
+	}
+	update_pid(&driver);
+	return true;
+}
+
+void updateSelfPosition(bool updateGyro) {
+//Updates position from RF if new data has been received
+	if (x != objects[0].x && y != objects[0].y) {
+		x = objects[0].x;
+		y = objects[0].y;
+		if (updateGyro) {
+			angle = RF_TO_DEGREES(objects[0].theta);
+			gyro_set_degrees(angle);
+		}
+	}
+	last_update_time = get_time();
+}
+
+void updateTargetPosition() {
+//Currently, chase after mouse bot.
+	target_x = objects[2].x;
+	target_y = objects[2].y;
+	angle_to_target = getTargetAngle(target_x,target_y);
+}
+/*
+
+void rfUpdate(bool updateGyro = true) {
+	updateSelfPosition(updateGyro);
+	last_update_time = get_time();
+}
+*/
+
+void activateRun() {
+/*
+* Called upon the mouse entering run away mode.
+*/
+	state = ACTIVATED;
+	return;
+}
+
+void step() {
+	switch(state) {
+		case ACTIVATED:
+			printf("\nActivated");
+			updateSelfPosition(true);
+			updateTargetPosition();
+			state = TURNING;
+			break;
+		case TURNING:
+			printf("\nTurning");
+			if (!turnToTarget()) {
+				state = DRIVING;
+				pause(500);
+				updateSelfPosition(true);
+				resetPID(angle_to_target);
+				state_time = get_time();
+			}
+			break;
+		case DRIVING:
+		
+			printf("\nDriving");
+			if (!driveToTarget()) {
+				state = HALT;
+			}
+			break;
+		default:
+			break;
+	}
+	//update_pid(&driver);
+	pause(50);
+}
+
+int umain(void) {
+	activateRun();
+	while(1) {
+		step();
+	}
+	return 0;
+}
