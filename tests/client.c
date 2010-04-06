1abab3dcc7c17a3ef4b7929c6a6dfd02ecbeeaaf
sgherbst <sgherbst@d6c55bfe-47cd-0eed-8827-52b0d3839261>
2010-04-06 23:55:42 +0000

    added some preliminary code to the main joyos dir to interface with simulator using sockets
    git-svn-id: http://joyos.googlecode.com/svn/trunk@265 d6c55bfe-47cd-0eed-8827-52b0d3839261

---
+++ tests/client.c
@@ -0,0 +1,40 @@
+#include <joyos.h>
+
+// robot port assignments
+#define LEFT_MOTOR 0 
+#define RIGHT_MOTOR 1
+
+void turnLeft(){
+  float startTheta = gyro_get_degrees();
+  motor_set_vel(LEFT_MOTOR, -50);
+  motor_set_vel(RIGHT_MOTOR, 50);
+  while((gyro_get_degrees() - startTheta) < M_PI/2.){
+    pause(25);
+  }
+}
+
+void forward(){
+  motor_set_vel(LEFT_MOTOR, 255);
+  motor_set_vel(RIGHT_MOTOR, 255);
+}
+
+int usetup(){
+
+	return 0;
+
+}
+
+int umain(){
+  int sign = 1;
+  
+  while(1){
+    
+    if (isStuck()) turnLeft();
+  
+    forward();
+    
+    pause(100);
+  
+  }
+}
+
